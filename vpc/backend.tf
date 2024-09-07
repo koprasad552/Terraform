3e32b0ef9e6b68a64a41436d8877176333e69ef1
prasadkoppula <prasadkoppula552@gmail.com>
2024-09-07 21:58:32 +0530

    added

--- vpc/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "kona-ganesh"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
-  }
-}
